874b4acdb3905d8ec2c93019d6b5ee67f1f252bf
Jovana417 <jovana.cruzcruz20@gmail.com>
2025-09-17 19:41:38 -0600

    Inciso a)

---
+++ Practica 01/Inciso a).js
@@ -0,0 +1,11 @@
+
+const nombre = "Armando";
+let edad= 25;
+
+
+
+var saludo = "Hola, " + nombre +". Tienes " + edad + " años.";
+
+console.log(saludo);
+
+//muestra en consola en contenido de saludo
